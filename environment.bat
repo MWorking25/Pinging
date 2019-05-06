0365091fab11cee3d34336e03b6335a85b01c9e8
Mayur Mhatre <mhatremayur2520@gmail.com>
2019-05-06 15:34:49 +0530

    Initialise project

---
+++ environment.bat
@@ -0,0 +1,8 @@
+set PHASE=developement
+set ENCRYPTED_KEY=FZDHuXvT2bSp5Mh4aDxUHJ8hshy8nGFtHpnnnOOAa3WIDTMfuqSU_MMaa=
+set JWT_SECRATE=ttJJHSH_2NNDH8ndjddd_kk-dky89lddowb&&hs6syvbddddvdiwodmd=
+set SERVING_PORT=8088
+set DB_NAME=pinging
+set DB_USER=root
+set DB_PASSWORD=
+set DB_HOST=localhost
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var bodypareser = require('body-parser')
+var fs = require('fs');
+var routes = require('./lib/routes');
+
+var app = express();
+
+app.use(bodypareser.urlencoded({limit:'5mb',extended:true}));
+app.use(bodypareser.json({limit:'5mb'}));
+	
+app.use(express.static(path.join(__dirname,'app')));
+routes.configure(app);
+
+
+var server = app.listen(parseInt(process.env.SERVING_PORT),function(){
+	console.log('server start on '+ server.address().port+ ' port');
+})	
+
+
+
