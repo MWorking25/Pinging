884eb261db1fc680ccb9c0d10b7fe2d6f0900625
Mayur Mhatre <mhatremayur2520@gmail.com>
2019-05-13 17:48:53 +0530

    Changes

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -2,8 +2,7 @@ angular.module('MyApp')
 	.controller('LoginController', ['$scope', '$http', '$route', '$location', '$window', '$timeout', 'Upload', 'socket', function ($scope, $http, $route, $location, $window, $timeout, Upload, socket) {
 
 
-        $scope.Connecteddevices = [{ip:'172.172.172.126',port:23},{ip:'172.172.172.228',port:23},{ip:'103.252.7.5'},{ip:'103.252.7.25'}]
-
+    
             $scope.NetworkStatus = function() {
                 socket.emit('Init', $scope.Connecteddevices);
             };
